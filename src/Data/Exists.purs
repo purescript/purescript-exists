529fa3baf89b59ec6d69cbe7cc8fe002c2af0233
yukikurage <55534323+yukikurage@users.noreply.github.com>
2022-11-17 00:33:24 +0900

    Fix documents

--- src/Data/Exists.purs
+++ src/Data/Exists.purs
@@ -33,10 +33,10 @@ type role Exists representational
 -- | The `mkExists` function is used to introduce a value of type `Exists f`, by providing a value of
 -- | type `f a`, for some type `a` which will be hidden in the existentially-quantified type.
 -- |
--- | For example, to create a value of type `Stream Number`, we might use `mkExists` as follows:
+-- | For example, to create a value of type `Stream Int`, we might use `mkExists` as follows:
 -- |
 -- | ```purescript
--- | nats :: Stream Number
+-- | nats :: Stream Int
 -- | nats = mkExists $ StreamF 0 (\n -> Tuple (n + 1) n)
 -- | ```
 mkExists :: forall f a. f a -> Exists f
